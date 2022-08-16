6cc62ed6e40d7e85dd7832e05e30b9832e8908a2
Lukasz Wilanski <lukasz.wilanski@gmail.com>
2022-08-16 14:25:15 +0200

    add funtion to render map based on divs

--- game.js
+++ game.js
@@ -1,7 +1,50 @@
-initGame();
+let snakeBoard = document.querySelector(".snakeBoard");
+let boxes = document.getElementsByClassName("box");
+let modul = document.querySelector('.modul');
+let start = document.querySelector('.start')
 
-function initGame() {
+let table = {
+    rowsColumns: 21,
+    box: 21*21
+};
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+let snake = {
+    direction: "right",
+    position: [[9,10],[10,10]],
+    food: 0, 
+    score: 0, 
+    time: 0,
+    canRotate: 0,
+    interval: 100,
+    init: function () {
+        snake.direction = "right";
+        snake.position = [[9,10],[10,10]];
+        snake.interval = 100;
+        snake.food = 0;
+        snake.score = 0;
+        snake.time = 0;
+        snakeBoard.innerHTML = "";
+        createTable();
+        renderSnake();
+    }
+ };
 
-}
+ snake.init();
+ 
+// creating table
+function createTable(){
+    if(snakeBoard.innerHTML === "") {
+        for(let i = 0; i < table.box; i++ ){
+            let divElement = document.createElement("div");
+            divElement.classList.add("box");
+            snakeBoard.appendChild(divElement);
+        };
+    };
+};
+
+// render snake
+function renderSnake() {
+    for(let i = 0; i < snake.position.length; i++) {
+        boxes[snake.position[i][0] + snake.position[i][1] * table.rowsColumns].classList.add("snake");
+    };
+};
